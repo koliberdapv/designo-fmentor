ad1f10bcf6a46c51a9fca40fe59e1c01f6a53ee9
koliberdapv <canrando4567@gmail.com>
2023-11-04 20:23:51 +0200

    styling base

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
 function App() {
-  return <h1>App</h1>;
+  return (
+    <div>
+      <h1 className="h1">The quick brown fox jumps over the lazy dog</h1>
+      <br />
+      <br />
+      <h1 className="h2">The quick brown fox jumps over the lazy dog</h1>
+      <br />
+      <br />
+      <h1 className="h3">The quick brown fox jumps over the lazy dog</h1>
+      <br />
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus
+        hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet
+        vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin
+        laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu
+        nibh. Nullam mollis. Ut justo. Suspendisse potenti.Sed egestas, ante et
+        vulputate volutpat, eros pede semper est, vitae luctus metus libero eu
+        augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida
+        id, est. Sed lectus. Praesent elementum hendrerit tortor. Sed semper
+        lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque
+        euismod dui, eu pulvinar nun. Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh nec
+        urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed
+        pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo,
+        eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo. Suspendisse
+        potenti.
+      </p>
+      <br />
+      <br />
+      <button className="btn">
+        <p className="btn-p">learn more</p>
+      </button>
+      <br />
+      <br />
+      <button className="btn btn-light">
+        <p className="btn-p">learn more</p>
+      </button>
+    </div>
+  );
 }
 
 export default App;
