47bc1d49f371ac2fbedb30cf3c2cbbe348342273
koliberdapv <canrando4567@gmail.com>
2023-11-21 16:53:56 +0200

    CTA fixed and finished

--- src/components/ui/SharedLayout.jsx
+++ src/components/ui/SharedLayout.jsx
@@ -1,22 +1,23 @@
-import { Footer, Header, Sidebar } from './';
+import { CTA, Footer, Header } from './';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 
 const SharedLayout = () => {
-	return (
-		<Wrapper>
-			<Header />
-			<Outlet />
-			<Footer />
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <Header />
+      <Outlet />
+      <CTA />
+      <Footer />
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
-	display: grid;
-	grid-template-rows: auto 1fr auto;
-	min-height: 100vh;
-	min-height: 100svh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+  min-height: 100svh;
 `;
 
 export default SharedLayout;
